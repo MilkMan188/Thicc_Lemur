b545e3b7172abffcf1c323bc56bac8ce9816e2a9
juaco188 <aqualung188@gmail.com>
2025-01-13 02:56:53 -0300

    Added a fix on main.py for when it's execute outside of it's environment.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 from PySide6.QtWidgets import QApplication
 from gui.gui import Widget
+import os
 import sys
 
 app = QApplication(sys.argv)
 
 gif_path = "img/phaesia_twerk2.gif"
+script_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(script_dir)
 
 window = Widget(gif_path)
 window.show()
